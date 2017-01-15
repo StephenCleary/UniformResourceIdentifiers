80793a3510ca4c0de5106dfb9861d36e9480a00b
Stephen Cleary <github.ourteddybear@xoxy.net>
2017-01-15 00:17:32 -0500

    Port string normalization - removing leading zeroes.

--- src/Nito.UniformResourceIdentifiers.Core/UniformResourceIdentifierReference.cs
+++ src/Nito.UniformResourceIdentifiers.Core/UniformResourceIdentifierReference.cs
@@ -43,10 +43,22 @@ namespace Nito.UniformResourceIdentifiers
             Scheme = scheme?.ToLowerInvariant();
             UserInfo = userInfo;
             Host = host?.ToLowerInvariant();
-            Port = port;
+            Port = NormalizePort(port);
             PathSegments = segments;
             Query = query;
             Fragment = fragment;
+        }
+
+        /// <summary>
+        /// Removes leading zeroes from the port string, but leaves one if the port is 0.
+        /// </summary>
+        /// <param name="port">The port string.</param>
+        private static string NormalizePort(string port)
+        {
+            if (string.IsNullOrEmpty(port))
+                return port;
+            var result = port.TrimStart('0');
+            return result == "" ? "0" : result;
         }
 
         /// <summary>
