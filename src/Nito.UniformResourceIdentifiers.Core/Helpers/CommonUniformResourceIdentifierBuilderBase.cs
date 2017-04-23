43f32b280bdef8bbcd038c9f0432efe13fb7ffe2
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-04-23 16:33:06 -0400

    Add ICommonBuilder.

--- src/Nito.UniformResourceIdentifiers.Core/Helpers/CommonUniformResourceIdentifierBuilderBase.cs
+++ src/Nito.UniformResourceIdentifiers.Core/Helpers/CommonUniformResourceIdentifierBuilderBase.cs
@@ -84,6 +84,14 @@ namespace Nito.UniformResourceIdentifiers.Helpers
     }
 
     /// <summary>
+    /// A builder with common options.
+    /// </summary>
+    /// <typeparam name="T">The type of the builder.</typeparam>
+    public interface ICommonBuilder<out T> : IBuilderWithUserInfo<T>, IBuilderWithHost<T>, IBuilderWithPort<T>, IBuilderWithPath<T>, IBuilderWithQuery<T>, IBuilderWithFragment<T>
+    {
+    }
+
+    /// <summary>
     /// Extension methods for builders.
     /// </summary>
     public static class BuilderExtensions
@@ -126,8 +134,7 @@ namespace Nito.UniformResourceIdentifiers.Helpers
     /// <summary>
     /// A builder base type for common <see cref="UniformResourceIdentifierReference"/>-derived types.
     /// </summary>
-    public abstract class CommonUniformResourceIdentifierBuilderBase<T>: IBuilderWithUserInfo<T>, IBuilderWithHost<T>, IBuilderWithPort<T>,
-        IBuilderWithPath<T>, IBuilderWithQuery<T>, IBuilderWithFragment<T>
+    public abstract class CommonUniformResourceIdentifierBuilderBase<T>: ICommonBuilder<T>
         where T : CommonUniformResourceIdentifierBuilderBase<T>
     {
         /// <summary>
@@ -293,6 +300,51 @@ namespace Nito.UniformResourceIdentifiers.Helpers
         protected virtual void ApplyUriReference(string uri, string expectedScheme)
         {
             var scheme = ApplyUriReference(uri);
+            if (scheme != expectedScheme)
+                throw new InvalidOperationException($"URI scheme \"{scheme}\" does not match expected scheme \"{expectedScheme ?? ""}\" in URI \"{uri}\".");
+        }
+    }
+
+    /// <summary>
+    /// Utility methods for builders.
+    /// </summary>
+    public static class BuilderUtils
+    {
+        /// <summary>
+        /// Deconstructs a URI reference into this builder.
+        /// </summary>
+        /// <param name="builder">The builder to modify.</param>
+        /// <param name="uri">The URI reference to deconstruct.</param>
+        public static T ApplyUriReference<T>(T builder, IUniformResourceIdentifierReference uri)
+            where T : ICommonBuilder<T>
+        {
+            return builder.WithUserInfo(uri.UserInfo).WithHost(uri.Host).WithPort(uri.Port).WithPrefixlessPathSegments(uri.PathSegments).WithQuery(uri.Query).WithFragment(uri.Fragment);
+        }
+
+        /// <summary>
+        /// Parses and deconstructs a URI reference into this builder. Returns the scheme of the URI reference, if any.
+        /// </summary>
+        /// <param name="builder">The builder to modify.</param>
+        /// <param name="uri">The URI reference to deconstruct.</param>
+        private static string ApplyUriReference<T>(T builder, string uri)
+            where T : ICommonBuilder<T>
+        {
+            Parser.ParseUriReference(uri, out string scheme, out string userInfo, out string host, out string port, out IReadOnlyList<string> pathSegements,
+                out string query, out string fragment);
+            builder.WithUserInfo(userInfo).WithHost(host).WithPort(port).WithPrefixlessPathSegments(pathSegements).WithQuery(query).WithFragment(fragment);
+            return scheme;
+        }
+
+        /// <summary>
+        /// Parses and deconstructs a URI reference into this builder, and verifies that the URI scheme matches what was expected.
+        /// </summary>
+        /// <param name="builder">The builder to modify.</param>
+        /// <param name="uri">The URI reference to deconstruct.</param>
+        /// <param name="expectedScheme">The expected URI scheme.</param>
+        public static void ApplyUriReference<T>(T builder, string uri, string expectedScheme)
+            where T : ICommonBuilder<T>
+        {
+            var scheme = ApplyUriReference(builder, uri);
             if (scheme != expectedScheme)
                 throw new InvalidOperationException($"URI scheme \"{scheme}\" does not match expected scheme \"{expectedScheme ?? ""}\" in URI \"{uri}\".");
         }
--- src/Nito.UniformResourceIdentifiers.Core/RelativeReferenceBuilder.cs
+++ src/Nito.UniformResourceIdentifiers.Core/RelativeReferenceBuilder.cs
@@ -9,8 +9,11 @@ namespace Nito.UniformResourceIdentifiers
     /// <summary>
     /// Builder for relative references.
     /// </summary>
-    public sealed class RelativeReferenceBuilder : CommonUniformResourceIdentifierBuilderBase<RelativeReferenceBuilder>
+    public sealed class RelativeReferenceBuilder : ICommonBuilder<RelativeReferenceBuilder>
     {
+        private string _userInfo, _host, _port, _query, _fragment;
+        private readonly List<string> _pathSegments = new List<string>();
+
         /// <summary>
         /// Constructs an empty builder.
         /// </summary>
@@ -22,7 +25,7 @@ namespace Nito.UniformResourceIdentifiers
         /// <param name="relativeReference">The relative reference used to set the builder's initial values.</param>
         public RelativeReferenceBuilder(RelativeReference relativeReference)
         {
-            ApplyUriReference(relativeReference);
+            BuilderUtils.ApplyUriReference(this, relativeReference);
         }
 
         /// <summary>
@@ -31,12 +34,71 @@ namespace Nito.UniformResourceIdentifiers
         /// <param name="relativeReference">The relative reference used to set the builder's initial values.</param>
         public RelativeReferenceBuilder(string relativeReference)
         {
-            ApplyUriReference(relativeReference, null);
+            BuilderUtils.ApplyUriReference(this, relativeReference, null);
         }
 
         /// <summary>
         /// Builds the relative reference.
         /// </summary>
-        public RelativeReference Build() => new RelativeReference(UserInfo, Host, Port, PathSegments, Query, Fragment);
+        public RelativeReference Build() => new RelativeReference(_userInfo, _host, _port, _pathSegments, _query, _fragment);
+
+        RelativeReferenceBuilder IBuilderWithUserInfo<RelativeReferenceBuilder>.WithUserInfo(string userInfo)
+        {
+            _userInfo = userInfo;
+            return this;
+        }
+
+        /// <summary>
+        /// Applies the host portion of the authority to this builder, overwriting any existing host.
+        /// </summary>
+        /// <param name="host">The host. May be <c>null</c> or the empty string.</param>
+        public RelativeReferenceBuilder WithHost(string host)
+        {
+            _host = host;
+            return this;
+        }
+
+        /// <summary>
+        /// Applies the port portion of the authority to this builder, overwriting any existing port.
+        /// </summary>
+        /// <param name="port">The port. May be <c>null</c> or the empty string.</param>
+        public RelativeReferenceBuilder WithPort(string port)
+        {
+            _port = port;
+            return this;
+        }
+
+        /// <summary>
+        /// Applies the path to this builder, overwriting any existing path. This method does not automatically prefix a forward slash to the resulting path.
+        /// </summary>
+        /// <param name="pathSegments">The path segments.</param>
+        public RelativeReferenceBuilder WithPrefixlessPathSegments(IEnumerable<string> pathSegments)
+        {
+            if (pathSegments == null)
+                throw new ArgumentNullException(nameof(pathSegments));
+            _pathSegments.Clear();
+            _pathSegments.AddRange(pathSegments);
+            return this;
+        }
+
+        /// <summary>
+        /// Applies the query string to this builder, overwriting any existing query.
+        /// </summary>
+        /// <param name="query">The query.</param>
+        public RelativeReferenceBuilder WithQuery(string query)
+        {
+            _query = query;
+            return this;
+        }
+
+        /// <summary>
+        /// Applies the fragment string to this builder, overwriting any existing fragment.
+        /// </summary>
+        /// <param name="fragment">The fragment.</param>
+        public RelativeReferenceBuilder WithFragment(string fragment)
+        {
+            _fragment = fragment;
+            return this;
+        }
     }
 }
